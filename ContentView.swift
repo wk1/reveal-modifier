b51c13e6ab3fa4bd433570910136d4fb0a31a048
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-09-15 13:54:38 +0200

    Add list with basic contetn

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+  var body: some View {
+    List {
+      ForEach(1...30, id: \.self) { num in
+        Text("Row \(num)")
+          .padding()
+          .listRowInsets(EdgeInsets())
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+          .background(.yellow)
+          .listRowBackground(Color.clear)
+      }
     }
+  }
 }
