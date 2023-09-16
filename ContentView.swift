71b3c9bde0cce1782e367be2801fef159b85d3f0
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-09-16 12:51:33 +0200

    Add leading view

--- ContentView.swift
+++ ContentView.swift
@@ -3,31 +3,41 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     List {
-      ForEach(1...30, id: \.self) { num in
-        Text("Row \(num)")
+      ForEach(1...30, id: \.self) { index in
+        Text("Row \(index)")
           .padding()
           .listRowInsets(EdgeInsets())
           .frame(maxWidth: .infinity, maxHeight: .infinity)
           .background(.yellow)
           .listRowBackground(Color.clear)
-          .reveal( trailing:  {
-            AnyView(
-              VStack(spacing: 5) {
-                HStack {
-                  Image(systemName: "stopwatch")
-                  Text("09:23")
+          .reveal( 
+            leading: {
+              AnyView (
+                Image(systemName: "trash")
+                  .padding()
+                  .font(.system(size: 12))
+                  .background(.red)
+              )
+            },
+            leadingAvailable: true,
+            trailing:  {
+              AnyView(
+                VStack(spacing: 5) {
+                  HStack {
+                    Image(systemName: "stopwatch")
+                    Text("09:23")
+                  }
+                  HStack {
+                    Image(systemName: "stopwatch.fill")
+                    Text("13:54")
+                  }
                 }
-                HStack {
-                  Image(systemName: "stopwatch.fill")
-                  Text("13:54")
-                }
-              }
-                .font(.system(size: 12))
-                .padding(10)
-                .background(.blue)
-            )
-          },
-                   trailingAvailable: true
+                  .font(.system(size: 12))
+                  .padding(10)
+                  .background(.blue)
+              )
+            },
+            trailingAvailable: true
           )
       }
     }
