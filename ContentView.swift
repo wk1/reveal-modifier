e9e79e4eebf8690507208498e1f027dd66ffa39a
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-09-17 12:45:21 +0200

    add reveal modifier (#3)
    * Add the reveal modifier
    
    * Remove bg color and debug print
    
    * Add check for leading/trailing availability
    
    * Prevent Animation if view is not available
    
    * Ignore file
    
    * Removed file that shouldn't be tracked
    
    * Add leading view

--- ContentView.swift
+++ ContentView.swift
@@ -3,13 +3,42 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     List {
-      ForEach(1...30, id: \.self) { num in
-        Text("Row \(num)")
+      ForEach(1...30, id: \.self) { index in
+        Text("Row \(index)")
           .padding()
           .listRowInsets(EdgeInsets())
           .frame(maxWidth: .infinity, maxHeight: .infinity)
           .background(.yellow)
           .listRowBackground(Color.clear)
+          .reveal( 
+            leading: {
+              AnyView (
+                Image(systemName: "trash")
+                  .padding()
+                  .font(.system(size: 12))
+                  .background(.red)
+              )
+            },
+            leadingAvailable: true,
+            trailing:  {
+              AnyView(
+                VStack(spacing: 5) {
+                  HStack {
+                    Image(systemName: "stopwatch")
+                    Text("09:23")
+                  }
+                  HStack {
+                    Image(systemName: "stopwatch.fill")
+                    Text("13:54")
+                  }
+                }
+                  .font(.system(size: 12))
+                  .padding(10)
+                  .background(.blue)
+              )
+            },
+            trailingAvailable: true
+          )
       }
     }
   }
